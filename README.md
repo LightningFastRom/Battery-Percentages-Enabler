bbd3dc39269aaee48fdd493331bc848d877ae54b
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-09-17 15:33:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Battery Percentages Enabler
---
+++ enable_battery_percentages.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+adb wait-for-device root
+adb wait-for-device remount 
+adb wait-for-device shell mkdir -p system/product/etc/res/images/charger/
+adb wait-for-device shell mkdir -p system/product/etc/res/values/charger/
+adb wait-for-device push images/*.png system/product/etc/res/images/charger/
+adb wait-for-device push anim/animation.txt system/product/etc/res/values/charger/animation.txt
+adb wait-for-device shell reboot -p
