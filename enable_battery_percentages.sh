c7149b13050dba92fff1f704a06d0affe3654e55
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2021-09-17 15:54:04 -0400

    Add ability to set a dpi group

--- enable_battery_percentages.sh
+++ enable_battery_percentages.sh
@@ -3,6 +3,6 @@ adb wait-for-device root
 adb wait-for-device remount 
 adb wait-for-device shell mkdir -p system/product/etc/res/images/charger/
 adb wait-for-device shell mkdir -p system/product/etc/res/values/charger/
-adb wait-for-device push images/*.png system/product/etc/res/images/charger/
+adb wait-for-device push images/$1/*.png system/product/etc/res/images/charger/
 adb wait-for-device push anim/animation.txt system/product/etc/res/values/charger/animation.txt
 adb wait-for-device shell reboot -p
